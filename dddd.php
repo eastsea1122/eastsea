7c4e0c17ac5fd5ae540eaa0c5d155f1542cf4ec9
eastsea1122 <dh8760@naver.com>
2025-04-18 11:29:58 +0900

    Add files via upload

---
+++ dddd.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>table_form_assignment</title>
+</head>
+<body>
+    <!-- <table border="1" align="center"> -->
+    <table align="center">
+        <tbody><tr>
+            <td colspan="3"><b>기본정보 입력</b></td>
+        </tr>
+        <tr>
+            <td rowspan="7"></td>
+            <td width="150">아이디</td>
+            <td><input type="text" size="40"></td>
+            <td><input type="button" value="중복확인"></td>
+        </tr>
+        <tr>
+            <td>비밀번호</td>
+            <td><input type="password" size="40"></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>비밀번호 확인</td>
+            <td><input type="password" size="40"></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td colspan="3">개인정보 입력</td>
+        </tr>
+        <tr>
+            <td>이름</td>
+            <td><input type="text" size="40"></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>이메일</td>
+            <td>
+                <input type="email" size="40"><br>
+                <input type="checkbox"> 이메일 수신 동의<br>
+            </td>
+            <td><input type="button" value="이메일인증"></td>
+        </tr>
+        <tr>
+            <td>휴대전화번호</td>
+            <td>
+                <input type="tel" size="40"><br>
+                <input type="checkbox"> SMS 수신 동의<br>
+            
+            </td>
+        </tr>
+    </tbody></table>
+<deepl-input-controller translate="no"></deepl-input-controller></body>
+<link rel="stylesheet" href="chrome-extension://cofdbpoegempjloogbagkncekinflcnj/build/content.css"><div dir="ltr" style="visibility: initial !important;"><div class="dl-input-translation-container svelte-95aucy"><div></div></div></div>
+<div dir="ltr" style="visibility: initial !important;"><div class="dl-input-translation-container svelte-95aucy"><div></div></div></div>
+<div class="dl-input-translation-container svelte-95aucy"><div></div></div>
+<div></div>
+<deepl-input-controller translate="no"></deepl-input-controller>
+</BODY>
+</html>
